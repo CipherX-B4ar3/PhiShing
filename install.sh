847f383c6ca58adc574a872e3ac155d851dc3787
CipherX <102590998+CipherX-MAN@users.noreply.github.com>
2022-05-04 11:27:39 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+apt-get update
+pkg install python3
+pkg install php
+apt-get install python3
+apt-get install php
+pip3 install base64
+pip3 install os
+pip3 install sys
+pip3 install subprocess
+
+
+python3 CipherX.py
